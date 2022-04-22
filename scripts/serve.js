e732d31387e8f8de239f7fd68bac6ff7cd702f06
Miroslav Jonas <missing.manual@gmail.com>
2022-04-22 14:27:56 +0200

    fix: fix http server

--- scripts/serve.js
+++ scripts/serve.js
@@ -1,13 +1,26 @@
 module.exports = {
     default: async () => {
-        const server = require('http-server').createServer({
-            root: './website'
+        const serve = require('child_process').execFile('npx', ['http-server', 'website'], {
+        });
+        process.on('exit', serve.kill());
+        process.on('SIGTERM', serve.kill());
+        serve.stdout.on('data', (chunk) => {
+            if (chunk.toString().indexOf('GET') === -1) {
+                process.stdout.write(chunk);
+            }
+        });
+        serve.stderr.on('data', (chunk) => {
+            process.stderr.write(chunk);
         });
 
-        server.listen(8080, () => {
-            console.log('Your website is available at http://localhost:8080');
+        return new Promise((res) => {
+            serve.on('exit', (code) => {
+                if (code == 0) {
+                    res({ success: true });
+                } else {
+                    res({ success: false });
+                }
+            });
         });
-
-        return { success: true };
     }
 };
